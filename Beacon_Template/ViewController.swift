be28a01b16ecb3cc8004bc0f2f3cb9e8f5794cad
Miguel Sicart <deterbold@gmail.com>
2018-09-24 20:44:46 +0200

    Initial Commit

---
+++ Beacon_Template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beacon_Template
+//
+//  Created by Miguel Sicart on 24/09/2018.
+//  Copyright © 2018 Miguel Sicart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
